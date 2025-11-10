8610bc9b0c383422b4625afd4ea863b3635f6237
Lara Owobowale <laraowobowale@gmail.com>
2025-11-10 16:06:31 +0100

    update

--- Q2&Q3.py
+++ Q2&Q3.py
@@ -2,37 +2,53 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-def makebestpolicy(V, alpha, days,order_p, p, h, d, x_max):
+def makebestpolicy(V, alpha, days,order_p, p, h, demand_prob, x_max):
     for t in range(days-2,-1,-1):
         for x in range(x_max+1):
             Q = []
-            for a in range(0,(x_max-x+1)):
-                
-                 # --- Case 1: order succeeds ---
-                stock_s = x + a
-                sales_s = min(d[t], stock_s)
-                new_stock_s = stock_s - sales_s
-                profit_s = p * sales_s - h * new_stock_s
-
-                # --- Case 2: order fails ---
-                stock_f = x
-                sales_f = min(d[t], stock_f)
-                new_stock_f = stock_f - sales_f
-                profit_fail = p * sales_f - h * new_stock_f
-
-                expected_profit = (
-                    order_p * (profit_s + V[new_stock_s, t + 1]) +
-                    (1 - order_p) * (profit_fail + V[new_stock_f, t + 1])
-                )
-                Q.append(expected_profit)                    
+            if x < x_max :
+                for a in [0,1]:
                     
-            a_min = 0
-            V[x, t] = Q[a_min]
-            alpha[x, t] = a_min
-            for a in range(0,(x_max-x+1)):
-                if Q[a] > V[x,t]:
-                    alpha[x,t] = a
-                    V[x,t] = Q[a]
+                    stock_s = x + a
+                    
+                    sales_s_1 = min(1, stock_s)
+                    
+                    new_stock_s_1 = stock_s - sales_s_1
+                    profit_s_1 = p * sales_s_1 - h * new_stock_s_1
+                    
+                    sales_s_0 = min(0, stock_s)
+                    new_stock_s_0 = stock_s - sales_s_0
+                    profit_s_0 = p * sales_s_0 - h * new_stock_s_0
+                    
+                
+                    if a ==0:
+                        expected_profit = (
+                        ((demand_prob*t)*profit_s_1 + V[new_stock_s_1, t + 1])+((1-demand_prob*t)*profit_s_0 + V[new_stock_s_0, t + 1])) 
+                    else:
+                        #failure case
+                        stock_f = x
+                        sales_f_1 = min(1, stock_f)
+                        new_stock_f_1 = stock_f - sales_f_1
+                        profit_fail_1 = p * sales_f_1 - h * new_stock_f_1
+                        
+                        stock_f = x
+                        sales_f_0 = min(0, stock_f)
+                        new_stock_f_0 = stock_f- sales_f_0
+                        profit_fail_0 = p * sales_f_0 - h * new_stock_f_0
+                        
+                        expected_profit = (
+                        order_p * (((demand_prob*t)*profit_s_1 + V[new_stock_s_1, t + 1])
+                                +((1-demand_prob*t)*profit_s_0 + V[new_stock_s_0, t + 1]))  +
+                        (1 - order_p) * ((demand_prob*t*profit_fail_1 + V[new_stock_f_1, t + 1]) + (1-demand_prob*t)*(profit_fail_0 + V[new_stock_f_0, t + 1])))
+                    Q.append(expected_profit)                    
+                        
+                a_min = Q.index(min(Q))
+                V[x, t] = Q[a_min]
+                alpha[x, t] = a_min
+                for a in [0,1]:
+                    if Q[a] > V[x,t]:
+                        alpha[x,t] = a
+                        V[x,t] = Q[a]
     print("Optimal Value Function V:\n", V)
     print("Optimal Policy alpha:\n", alpha)
     return V, alpha
@@ -64,12 +80,11 @@ def makePlot(V, alpha, days, x_max):
 
 
 
-def Simulation(runs,V, alpha,initial_stock, days,order_prob ,price_per_unit, holding_cost, x_max):
+def Simulation(runs,V, alpha,initial_stock, days,order_prob ,price_per_unit, holding_cost, x_max, demand_probability):
     total_profit = 0
     profit_list = []
     for _ in range(runs):
-        Demand = create_demand(days, 1/150)
-        V,alpha = makebestpolicy(V, alpha, days,order_prob ,price_per_unit, holding_cost, Demand, x_max)
+        V,alpha = makebestpolicy(V, alpha, days,order_prob ,price_per_unit, holding_cost, demand_probability, x_max)
         expected_profit = V[initial_stock, 0]
         total_profit += expected_profit
         print(f"Run {_+1}: Expected profit=${expected_profit:.2f}")
@@ -95,18 +110,18 @@ def main():
     price_per_unit = 1
     holding_cost = 0.1
     order_prob = 0.5
-    x_max = 10
+    x_max = days+initial_stock
     V = np.zeros((x_max+1,days))
     V[:, -1] = 0
     alpha = np.zeros((x_max+1,days))
-    Demand = create_demand(days, demand_prob)
-    V,alpha = makebestpolicy(V, alpha, days,order_prob ,price_per_unit, holding_cost, Demand, x_max)
-    makePlot(V, alpha, days, x_max)
+    #Demand = create_demand(days, demand_prob)
+    V,alpha = makebestpolicy(V, alpha, days,order_prob ,price_per_unit, holding_cost, demand_prob, x_max)
+    #makePlot(V, alpha, days, x_max)
     # Maximal expected reward starting from initial stock
     print("Maximal Expected Reward:", V[initial_stock, 0])
-    no_runs = 1000
-    data_profit = Simulation(runs=no_runs,V=V,alpha=alpha,initial_stock=initial_stock, days=days,order_prob =order_prob ,price_per_unit=price_per_unit, holding_cost=holding_cost,x_max=x_max)
-    makeHistogram(data_profit)
+    no_runs = 100
+    #data_profit = Simulation(runs=no_runs,V=V,alpha=alpha,initial_stock=initial_stock, days=days,order_prob =order_prob ,price_per_unit=price_per_unit, holding_cost=holding_cost,x_max=x_max, demand_probability=demand_prob)
+    #makeHistogram(data_profit)
     
     
 
